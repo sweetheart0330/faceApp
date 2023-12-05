a3483e8b78d89984ce9bdb0eb67160c49b028291
firelordnt <firelordnt@rambler.ru>
2023-12-05 14:43:12 +0300

    rewrote design.py, added some functions in main.py

--- main.py
+++ main.py
@@ -12,15 +12,19 @@ class MyWin(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.initui()
+        self.init_ui()
+        self.eyeColor = None
+        self.lipsColor = None
 
-    def initui(self):
+    def init_ui(self):
         self.ui.img.setAlignment(Qt.AlignCenter)
-        self.ui.comboBox.addItems(['Цензура', 'Blush', 'Дьявол', 'Собака', 'Очки', 'Шляпа', 'Шляпа и очки', 'Сердце', 'Мейкап', 'Moustache'])
+        self.ui.comboBox.addItems(['', 'Цензура', 'Blush', 'Дьявол', 'Собака', 'Очки', 'Шляпа', 'Шляпа и очки', 'Сердце', 'Мейкап', 'Moustache'])
         self.ui.comboBox.activated[int].connect(self.checked)
-        self.ui.importFile.clicked.connect(self.click)
+        self.ui.importFile.clicked.connect(self.choice_file)
+        self.ui.pickEyesColor.clicked.connect(self.eyes_color_click)
+        self.ui.pickLipsColor.clicked.connect(self.lips_color_click)
 
-    def click(self):
+    def choice_file(self):
         path = QtWidgets.QFileDialog.getOpenFileName()[0]
         im = Image.open(path)
         im = im.convert("RGBA")
@@ -35,10 +39,38 @@ class MyWin(QtWidgets.QMainWindow):
         else:
             pixmap = pixmap.scaledToWidth(w)
         self.ui.img.setPixmap(pixmap)
+        color = QtWidgets.QColorDialog.getColor()
+        print(color.red(), color.green(), color.blue(), color.alpha())
+
+    def eyes_color_click(self):
+        color = QtWidgets.QColorDialog.getColor()
+        self.eyeColor = color
+        print('-', self.eyeColor.red(), self.eyeColor.green(), self.eyeColor.blue(), self.eyeColor.alpha())
+
+    def lips_color_click(self):
+        color = QtWidgets.QColorDialog.getColor()
+        self.lipsColor = color
+        print('+', self.lipsColor.red(), self.lipsColor.green(), self.lipsColor.blue(), self.lipsColor.alpha())
 
     def checked(self, item):
-        if item == 8:
+        if item == 1:
             pass
+        elif item == 2:
+            pass
+        elif item == 3:
+            pass
+        elif item == 4:
+            pass
+        elif item == 5:
+            pass
+        elif item == 6:
+            pass
+        elif item == 7:
+            pass
+        elif item == 8:
+            pass
+        elif item == 9:
+            print('make')
 
 
 app = QtWidgets.QApplication(sys.argv)
