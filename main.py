09142a97975b9ef857ac6f98fadb5b5a1166add8
firelordnt <firelordnt@rambler.ru>
2023-12-06 00:35:20 +0300

    [ADD] save button

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox)
 from PyQt5.QtCore import Qt
 
-from test import hat_filter
+from Black_Rect_Filter import brect_filter
+from Blush_Filter import blush_filter
+from Devil_Filter import devil_filter
+from Dog_Filter import dog_filter
+from Glasses_Filter import glasses_filter
+from Hat_Filter import hat_filter
+from Hat_And_Glasses_Filter import hat_glas_filter
+from Hearts_Filter import hearts_filter
+from Make_Up import make_up_filter
+from Moustache_Filter import moust_filter
 
 
 class MyWin(QtWidgets.QMainWindow):
@@ -33,17 +42,29 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.pickEyesColor.clicked.connect(self.eyes_color_click)
         self.ui.pickLipsColor.clicked.connect(self.lips_color_click)
         self.ui.buttonProcess.clicked.connect(self.process)
+        self.ui.buttonSave.clicked.connect(self.saveProcess)
         self.ui.makeUp.setVisible(False)
 
-    def make_up_filter(self):
-        pass
+    def saveProcess(self):
+        im = Image.fromarray(self.imgPostProcess)
+        b, g, r = im.split()
+        im = Image.merge("RGB", (r, g, b))
+        im2 = im.convert("RGBA")
+        path = QtWidgets.QFileDialog.getSaveFileName(initialFilter="Images (*.png *.jpg *.jpeg)", filter="Images (*.png *.jpg *.jpeg)")[0]
+        im2.save(path)
 
     def process(self):
         if self.picked_filter is None:
             self.ui.imgPostProcess.setPixmap(QPixmap())
-        elif self.picked_filter == self.make_up_filter:
-            lipsColorRgb = [self.lipsColor.red(), self.lipsColor.green(), self.lipsColor.blue()]
-            eyesColorRgb = [self.eyesColor.red(), self.eyesColor.green(), self.eyesColor.blue()]
+        elif self.picked_filter == make_up_filter:
+            if self.ui.lipsCheck.isChecked():
+                lipsColorRgb = [self.lipsColor.blue(), self.lipsColor.green(), self.lipsColor.red()]
+            else:
+                lipsColorRgb = [0, 0, 0]
+            if self.ui.eyesCheck.isChecked():
+                eyesColorRgb = [self.eyesColor.blue(), self.eyesColor.green(), self.eyesColor.red()]
+            else:
+                eyesColorRgb = [0, 0, 0]
             self.imgPostProcess = self.picked_filter(self.ui.lipsCheck.isChecked(), self.ui.eyesCheck.isChecked(),
                                self.ui.chbCheck.isChecked(), lipsColorRgb, eyesColorRgb, self.path)
         else:
@@ -57,7 +78,6 @@ class MyWin(QtWidgets.QMainWindow):
             pixmap = pixmap.scaledToWidth(w)
         self.ui.imgPostProcess.setPixmap(pixmap)
 
-
     def pil2pixmap(self, image):
         '''if im.mode == "RGB":
             r, g, b = im.split()
@@ -67,7 +87,10 @@ class MyWin(QtWidgets.QMainWindow):
             im = Image.merge("RGBA", (b, g, r, a))
         elif im.mode == "L":
             im = im.convert("RGBA")'''
-        im = Image.fromarray(image)
+        if not (type(image) is Image.Image):
+            im = Image.fromarray(image)
+        else:
+            im = image
         im2 = im.convert("RGBA")
         data = im2.tobytes("raw", "RGBA")
         qim = QtGui.QImage(data, im.size[0], im.size[1], QtGui.QImage.Format_ARGB32)
@@ -75,7 +98,7 @@ class MyWin(QtWidgets.QMainWindow):
         return pixmap
 
     def choice_file(self):
-        self.path = QtWidgets.QFileDialog.getOpenFileName()[0]
+        self.path = QtWidgets.QFileDialog.getOpenFileName(filter="Images (*.png *.jpg *.jpeg)")[0]
         pixmap = QPixmap(self.path)
         '''test = hat_filter(self.path)
         image = Image.fromarray(test)
@@ -95,7 +118,7 @@ class MyWin(QtWidgets.QMainWindow):
         image = Image.new("RGB", (self.ui.eyesColorShower.width()-3, self.ui.eyesColorShower.height()-3),
                           (200, 200, 200))
         ImageDraw.Draw(image).rectangle(((0, 0), image.size),
-                                        fill=(self.eyeColor.blue(), self.eyeColor.green(), self.eyeColor.red()))
+                                        fill=(self.eyesColor.blue(), self.eyesColor.green(), self.eyesColor.red()))
         pixmap = self.pil2pixmap(image)
         self.ui.eyesColorShower.setPixmap(pixmap)
 
